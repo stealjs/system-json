35c9843adb8e287fa425090a9a4ee03ec4e1261e
Justin Meyer <justinbmeyer@gmail.com>
2014-12-26 23:03:46 -0600

    removing locate hook

--- json.js
+++ json.js
@@ -6,16 +6,12 @@ function _SYSTEM_addJSON(loader) {
 
   // if someone has a moduleName that is .json, make sure it loads a json file
   // no matter what paths might do
-  var loaderLocate = loader.locate;
-  loader.locate = function(load){
-    return loaderLocate.apply(this, arguments).then(function(address){
-      if(load.name.lastIndexOf(".json") == load.name.length - 5) {
-        // remove .js from address
-        address = address.replace(/\.js$/,"")
-      }
-      return address;
-    });
-  };
+  //var loaderLocate = loader.locate;
+  //loader.locate = function(load){
+  //  return loaderLocate.apply(this, arguments).then(function(address){
+  //    return address;
+  //  });
+  //};
 
   var loaderInstantiate = loader.instantiate;
   loader.instantiate = function(load) {
@@ -27,12 +23,14 @@ function _SYSTEM_addJSON(loader) {
       try{
         parsed = JSON.parse(load.source);
       } catch(e) {}
-      
-      load.metadata.format = 'json';
+      if(parsed) {
+        load.metadata.format = 'json';
 
-      load.metadata.execute = function(){
-        return parsed;
-      };
+	      load.metadata.execute = function(){
+	        return parsed;
+	      };
+      }
+      
 
     }
     return loaderInstantiate.call(loader, load);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 QUnit.module("system-json plugin");
 
 asyncTest("Basics works", function(){
+	System.paths["test/my.json"] = "test/my.json";
 	System.import("test/my.json").then(function(my){
 		equal(my.name, "foo", "name is right");
 	}).then(start);
